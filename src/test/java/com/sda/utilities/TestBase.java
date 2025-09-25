398399e96b97960cb6ff1e53c9a8e19ad450d7aa
Hanaa011 <hanaasaloufi@gmail.com>
2025-09-25 23:24:38 +0300

    Add initial project structure with TestNG and Selenium tests

---
+++ src/test/java/com/sda/utilities/TestBase.java
@@ -0,0 +1,45 @@
+package com.sda.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser) {
+        System.out.println("🚀 Launching browser: " + browser);
+
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.setAcceptInsecureCerts(true); // ✅ يسمح بتجاوز تحذير SSL
+                driver = new ChromeDriver(options);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("🧹 Closing browser...");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
